d3c1f40b5ec597d115a0c77696c6cc5424ce2aa9
Ana Carolina Santos <149174247+Carolrbs@users.noreply.github.com>
2025-11-24 13:53:14 -0300

    Add files via upload

---
+++ Led-main/Led-main/Leds/Leds/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Leds
+{
+    public partial class Form1 : Form
+    {
+        public Led Luz;
+        public Form1()
+        {
+            InitializeComponent();
+            Luz = new Led();
+            Atualizainterface()
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_on_8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbl_IndicarValor_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
